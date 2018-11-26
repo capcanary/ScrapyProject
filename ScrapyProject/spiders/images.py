7b58ed9dfd08f25f3189883ae7284b8598625e08
Chris <30950894+capcanary@users.noreply.github.com>
2018-11-26 17:20:22 +0800

    Update images.py

--- ScrapyProject/spiders/images.py
+++ ScrapyProject/spiders/images.py
@@ -12,7 +12,7 @@ class ImagesSpider(scrapy.Spider):
 
     headers = {
         "HOST": "www.aitaotu.com",
-        "Referer": "https://www.aitaotu.com/tag/gaogenxie.html",
+        "Referer": "https://www.aitaotu.com/weimei/",
         'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0"
     }
 
